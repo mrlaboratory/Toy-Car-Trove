0c789af6e0bda867b453359c0e88b2b82d49ac97
mrlaboratory <mrlaboratory23@gmail.com>
2023-05-18 11:14:03 +0600

    complate the login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthProvider';
 
 const Navbar = () => {
+  const { signOutUser, user } = useContext(AuthContext)
 
-const navitem = <> <NavLink to='/'>Home</NavLink>
-<NavLink to='/login'>Login</NavLink>
-<NavLink to='/register'>Register</NavLink>
-<NavLink to='/addcars'>Add cars</NavLink>
-</>
-  
+  const navitem = <> <NavLink to='/'>Home</NavLink>
+    <NavLink to='/login'>Login</NavLink>
+    <NavLink to='/register'>Register</NavLink>
+    <NavLink to='/addcars'>Add cars</NavLink>
+  </>
+
   return (
     <div className="navbar " id='navbar'>
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <label tabIndex={5} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-{navitem}
+          <ul tabIndex={5} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            {navitem}
 
           </ul>
         </div>
@@ -25,19 +27,27 @@ const navitem = <> <NavLink to='/'>Home</NavLink>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 font-bold gap-3 text-color">
-    
+
           {navitem}
-          
+
         </ul>
       </div>
       <div className="navbar-end " >
-     
+{
+  user ?  <div className="dropdown dropdown-end">
+  <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+    <div className="w-10 rounded-full border-2 border-primary">
+      <img className='w-full rounded-full ' src="https://mrlaboratory.github.io/img/user.png" />
+    </div>
+  </label>
+  <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+  
+    <li><a onClick={signOutUser} >Logout</a></li>
+  </ul>
+</div> : <Link to='login' className='btn btn-primary text-white'>Login</Link>
+}
+
        
-       
-            <img  className='w-12 ml-3 rounded-full border-2 border-primary ' src='https://mrlaboratory.github.io/img/user.png' alt="" />
-      
-       
-        
       </div>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,116 @@
-import React from 'react';
-import Spinner from '../components/Spinner';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { BiShow, BiHide } from 'react-icons/Bi';
+import { FcGoogle } from 'react-icons/Fc';
+import { AiFillGithub } from 'react-icons/Ai';
+import { toast } from 'react-hot-toast';
+import { AuthContext } from '../auth/AuthProvider';
 
+
+
+
+// this is login page 
 const Login = () => {
+
+    const { user, loginUser, loginWithGoogle, loginWithGithub } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const [isShow, setIsShow] = useState(false)
+
+
+    const location = useLocation()
+
+    const from = location?.state?.pathname || '/'
+    console.log(from)
+    // console.log(location)
+    const sigininuser = e => {
+        e.preventDefault()
+        setError('')
+        setSuccess('')
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        // console.log(email,password)
+        loginUser(email, password)
+            .then(res => {
+                setSuccess('Login successfull')
+                toast.success('Login successfull')
+
+            })
+            .catch(e => {
+                setError(e.message)
+                console.log(e.message)
+            })
+
+    }
+
+    useEffect(() => {
+        if (user) {
+            console.log(1)
+            navigate(from, { replace: true })
+        }
+    }, [user, from])
+    // if(user){
+    //   navigate(from)
+    // }
+
+
+
     return (
-        <div>
-            <h2>this is login page </h2>
-            <Spinner></Spinner>
+        <div className="">
+            <div className="flex justify-center my-5">
+
+                <div className="w-full sm:w-[500px] p-5">
+                    <div className='text-center'>
+                        <h2 className='text-xl font-bold my-3'> Login</h2>
+                    </div>
+                    <div className='w-full sm:w-[500px]'>
+                        <form onSubmit={sigininuser} className="card-body p-0 w-full">
+
+                            <div className="form-control">
+
+                                <input required type="email" placeholder="Your email " name='email' className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control ">
+
+                                <div className='relative'>
+                                    <input required type={`${isShow ? 'text' : 'password'}`} name='password' placeholder="Your password" className="input input-bordered w-full" />
+                                    <button className='absolute right-0 top-0 h-full pr-1' type='button' onClick={() => setIsShow(!isShow)}>{isShow ? <BiHide className='text-2xl'></BiHide> : <BiShow className='text-2xl'></BiShow>}</button>
+                                </div>
+                                <label className="label ">
+
+                                    <p>Are you new ?  <Link to='/register' className="text-primary">Create account </Link></p>
+                                </label>
+                                <label className='label'>
+                                    {error && <h3 className='text-red-600 font-bold'>{error}</h3>}
+                                    {success && <h3 className='text-green-600 font-bold'>{success}</h3>}
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <div className='flex justify-start gap-2 items-center'>
+                                <input type="checkbox" className="checkbox checkbox-primary" />
+                                    <span className="label-text">Remember me</span>
+                                </div>
+                            </div>
+                            <div className="form-control ">
+                                <button type='submit' className="btn btn-primary text-white">Login</button>
+                            </div>
+                            <div className='mb-5'>
+                                <h3 className='font-bold text-center'>Or</h3>
+                                <div className='mt-2 flex justify-between items-center border-2 border-primary rounded-lg'>
+                                    <button onClick={loginWithGoogle} type='button' className='flex justify-center items-center text-xl   flex-grow px-3 py-2 w-full'><FcGoogle className='text-2xl'></FcGoogle> Signin with Google</button>
+
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
